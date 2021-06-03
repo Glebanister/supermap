fd2dbfba37244536d524adbeebaba6c1fdfeeb2d
Glebanister <glebmar2001@gmail.com>
2021-06-03 21:19:45 +0300

    Implement BST

--- src/core/BST.hpp
+++ src/core/BST.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <string>
+#include <map>
 
 #include "KeyValueStorage.hpp"
 
@@ -9,21 +9,24 @@ namespace supermap {
 template <std::size_t KeyLen, std::size_t ValueLen>
 class BST : public KeyValueStorage<KeyLen, ValueLen> {
   public:
-    void add(const Key<KeyLen> &, ByteArray<ValueLen> &&) override {
-        throw NotImplementedException();
+    void add(const Key<KeyLen> &key, ByteArray<ValueLen> &&value) override {
+        map_[key] = std::move(value);
     }
 
-    void remove(const Key<KeyLen> &) override {
-        throw NotImplementedException();
+    void remove(const Key<KeyLen> &key) override {
+        map_.erase(key);
     }
 
-    bool containsKey(const Key<KeyLen> &) override {
-        throw NotImplementedException();
+    bool containsKey(const Key<KeyLen> &key) override {
+        return map_.find(key) != map_.end();
     }
 
-    const ByteArray<ValueLen> &getValue(const Key<KeyLen> &) override {
-        throw NotImplementedException();
+    const ByteArray<ValueLen> &getValue(const Key<KeyLen> &key) override {
+        return *map_.find(key);
     }
+
+  private:
+    std::map<Key<KeyLen>, ByteArray<ValueLen>> map_;
 };
 
 } // supermap
